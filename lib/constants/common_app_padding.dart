df310f6de3abd9620498bf5fc996e2bccd21d703
id320 <>
2022-10-13 19:41:36 +0300

    padd

---
+++ lib/constants/common_app_padding.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const stdpadding = 15.0;
+
+const stdHorizontalPadding = EdgeInsets.symmetric(horizontal: stdpadding);
+const stdHorizontalTopPadding = EdgeInsets.only(left: 15, right: 15, top: 15);
