f971e1e7d114e81a826b42a84f1d9c4a296aa628
id320 <>
2022-10-14 14:22:44 +0300

    url

---
+++ lib/utils/url_utils.dart
@@ -0,0 +1,102 @@
+bool isURL(String? str,
+    {List<String?> protocols = const ['http', 'https', 'ftp'],
+    bool requireTld = true,
+    bool requireProtocol = false,
+    bool allowUnderscore = false,
+    List<String> hostWhitelist = const [],
+    List<String> hostBlacklist = const []}) {
+  if (str == null || str.isEmpty || str.length > 2083 || str.startsWith('mailto:')) {
+    return false;
+  }
+
+  if (!str.contains('/')) return false;
+
+  var protocol, user, auth, host, hostname, port, port_str, path, query, hash, split;
+
+  // check protocol
+  split = str.split('://');
+  if (split.length > 1) {
+    protocol = shift(split);
+    if (protocols.indexOf(protocol) == -1) {
+      return false;
+    }
+  } else if (requireProtocol == true) {
+    return false;
+  }
+  str = split.join('://');
+
+  // check hash
+  split = str!.split('#');
+  str = shift(split);
+  hash = split.join('#');
+  if (hash != null && hash != "" && RegExp(r'\s').hasMatch(hash)) {
+    return false;
+  }
+
+  // check query params
+  split = str!.split('?');
+  str = shift(split);
+  query = split.join('?');
+  if (query != null && query != "" && RegExp(r'\s').hasMatch(query)) {
+    return false;
+  }
+
+  // check path
+  split = str!.split('/');
+  str = shift(split);
+  path = split.join('/');
+  if (path != null && path != "" && RegExp(r'\s').hasMatch(path)) {
+    return false;
+  }
+
+  // check auth type urls
+  split = str!.split('@');
+  if (split.length > 1) {
+    auth = shift(split);
+    if (auth.indexOf(':') >= 0) {
+      auth = auth.split(':');
+      user = shift(auth);
+      if (!RegExp(r'^\S+$').hasMatch(user)) {
+        return false;
+      }
+      if (!RegExp(r'^\S*$').hasMatch(user)) {
+        return false;
+      }
+    }
+  }
+
+  // check hostname
+  hostname = split.join('@');
+  split = hostname.split(':');
+  host = shift(split);
+  if (split.length > 0) {
+    port_str = split.join(':');
+    try {
+      port = int.parse(port_str, radix: 10);
+    } catch (e) {
+      return false;
+    }
+    if (!RegExp(r'^[0-9]+$').hasMatch(port_str) || port <= 0 || port > 65535) {
+      return false;
+    }
+  }
+
+  if (hostWhitelist.isNotEmpty && !hostWhitelist.contains(host)) {
+    return false;
+  }
+
+  if (hostBlacklist.isNotEmpty && hostBlacklist.contains(host)) {
+    return false;
+  }
+
+  return true;
+}
+
+shift(List l) {
+  if (l.isNotEmpty) {
+    var first = l.first;
+    l.removeAt(0);
+    return first;
+  }
+  return null;
+}
